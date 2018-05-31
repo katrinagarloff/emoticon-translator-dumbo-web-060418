45bee091986c33be88b75c75956dfad654f6ec1d
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-31 15:51:55 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file_path)
  og_hash = YAML::load_file(file_path)
  formatted_hash = Hash.new{|hsh,key| hsh[key] = {}}
  og_hash.each do |key, ar|
-   formatted_hash[key].store("")
+   formatted_hash["get"].store("")
  end
 end
 load_library("./lib/emoticons.yml")
